31bfb71b7df48d16810300ffa22a2a795e444cff
Jason Livesay <runvnc@gmail.com>
2022-03-28 18:16:38 -0500

    update

--- access.mjs
+++ access.mjs
@@ -6,6 +6,9 @@ export const getIndexer = (net) => {
     const port = ''
     const token = {'X-API-key' : 'VGgpKbdrVC3PQz4vCHMIS9UvoPHkLZJI16RWSOi9'}
     return new algosdk.Indexer(token, baseServer, port)
+  } else if (net == 'DEV') {
+    
+  
   } else {
     let net = 'https://algoindexer.testnet.algoexplorerapi.io'
     return new algosdk.Indexer('', net,'')
@@ -16,10 +19,15 @@ export const getIndexer = (net) => {
 export function getAlgod(which) {
   let net = 'https://node.algonfts.art'
   let token = '8854a3be0df4c5495a9e8f62ff7b0b74dc3fe197351bff3d66c4996201a912d0'
+  let port = ''
   if (which == 'TESTNET') {
     net = 'https://node.testnet.algoexplorerapi.io'
     token = ''
+  } else if (which == 'DEV') {
+    net = 'http://localhost'
+    token = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    port = 4001
   }
-  let algod = new algosdk.Algodv2(token, net,'')
+  let algod = new algosdk.Algodv2(token, net,port)
   return algod
 }
